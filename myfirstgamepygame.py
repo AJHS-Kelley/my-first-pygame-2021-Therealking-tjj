e1bb72628d6e564407ddd2805955075795c01d50
Terrance H <71032007+Therealking-tjj@users.noreply.github.com>
2021-11-30 14:44:59 -0500

    v0.3
    copy and pasted rectangle

--- myfirstgamepygame.py
+++ myfirstgamepygame.py
@@ -1,4 +1,4 @@
-# My first pygame, Terrance Holland, 11/30/21, 2:15, v0.2
+# My first pygame, Terrance Holland, 11/30/21, 2:15, v0.3
 
 import pygame, sys 
 from pygame.locals import*
@@ -9,5 +9,22 @@ pygame.init()
 #set up the game window.
 windowsurface = pygame.display. set_mode ((500, 400),0,32)
 pygame.display.set_captain('hello, world!')
+
+#setup color values
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = ( 0,215,0)
+BLUE = ( 0,0, 255)
+VIOLET = (125,255,25)
+
+#setup fonts,
+basicfonts = pygame.font. sysfont(none, 48)
+
+#setup text.
+text = basicFont. render('hello, world', True, WHITE,BLUE)
+textReact = text.get_react ()
+textReact.centrex = windowSurface.get_react().centerx
+textReact.centrex = windowSurface.get_react().centery
 
  
